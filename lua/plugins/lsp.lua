1ce6e3f51ae84db4ce50085bbb6c197bbd0324ff
Aashir Razzaq <aashir.razzaq10@gmail.com>
2025-05-04 13:50:19 +0500

    fix(lsp): fixed broken angularls configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -182,11 +182,11 @@ return {
         -- ts_ls = {}
 
         angularls = {
-            root_dir = function(fname)
-                local util = require("lspconfig.util")
-                return util.root_pattern("angular.json", "nx.json")(fname)
-            end,
-            filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
+            -- root_dir = function(fname)
+            --     local util = require("lspconfig.util")
+            --     return util.root_pattern("angular.json", "nx.json")(fname)
+            -- end,
+            -- filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
         },
 
         html = {
